8742e36c40525287069e4836e42c0e13b5f4a63d
Marie Amelie Sandrock <msandrock@audencia.com>
2019-01-26 17:19:50 +0100

    other exercices

--- firstPractice/exercise.py
+++ firstPractice/exercise.py
@@ -17,3 +17,29 @@ secondNb = int(input())
 
 maxNb = findMax(firstNb, secondNb)
 print("the Max number is: {}".format(maxNb))
+
+
+#write a program to print numbers from 1 to 10
+for number in range(1,11):
+  print(number, end = '')
+
+#write a program to print the sum of odd number from 1 to 10
+sumOfOdd = 0
+
+for number in range(1,11)
+  if number%2 == 0:
+    continue
+  else:
+    sumOfOdd += number
+
+#Write a program to check if a given number is prime or not
+
+number = 2
+
+for i in range(2, number)
+  if number%i = 0:
+    print("not prime")
+    break
+  else:
+    print("prime")
+
